60a6f2397929da9a17246aa2864bedf4ec95c4dd
xwilson03 <xanderwilson2014@gmail.com>
2024-07-28 06:47:44 -0500

    adds simple nameplate

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
+    import NamePlate from "./lib/NamePlate.svelte";
     import StatLine from "./lib/stats/StatLine.svelte";
 </script>
 
 <main class="flex h-screen w-screen bg-zinc-900 text-white">
-    <div>
+    <div class="flex flex-col">
+        <NamePlate />
         <StatLine />
     </div>
 </main>
---
+++ src/lib/NamePlate.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let name: string = "Name"
+    let race: string = "Race"
+    let class_name: string = "Class"
+    let level: number = 1
+</script>
+
+<section class="pane flex justify-center gap-x-1 p-1 align-middle">
+    <div class="text-center">{name}, {race} {class_name} {level}</div>
+</section>
