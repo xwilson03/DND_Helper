b0ce9d1bee70c88f7fe55ca0151811073d00de0b
xwilson03 <xanderwilson2014@gmail.com>
2024-07-28 06:30:14 -0500

    moves pane styling to tailwind class

--- src/lib/stats/StatLine.svelte
+++ src/lib/stats/StatLine.svelte
@@ -2,7 +2,7 @@
     import StatBox from "./StatBox.svelte";
 </script>
 
-<section class="flex gap-x-1 gap-y-3 bg-zinc-800 p-1 outline outline-1 outline-zinc-700">
+<section class="pane flex gap-x-1 gap-y-3">
     <StatBox stat_name="STR" />
     <StatBox stat_name="DEX" />
     <StatBox stat_name="CON" />
