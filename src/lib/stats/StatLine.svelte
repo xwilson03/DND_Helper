91ac01e95c60e1a191b8d1704c09b89295b01395
xwilson03 <xanderwilson2014@gmail.com>
2024-07-28 03:22:36 -0500

    changes statline spacing

--- src/lib/stats/StatLine.svelte
+++ src/lib/stats/StatLine.svelte
@@ -2,7 +2,7 @@
     import StatBox from "./StatBox.svelte";
 </script>
 
-<section class="flex flex-col items-stretch justify-around bg-zinc-800 px-1">
+<section class="flex flex-col items-stretch bg-zinc-800 px-1">
     <StatBox stat_name="STR" />
     <StatBox stat_name="DEX" />
     <StatBox stat_name="CON" />
