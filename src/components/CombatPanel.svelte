063686f7612210aac6816d8ccbd4e2c7c0f9fb4f
xwilson03 <xanderwilson2014@gmail.com>
2024-07-29 20:52:44 -0500

    adds statefulness to CombatPanel

--- src/components/CombatPanel.svelte
+++ src/components/CombatPanel.svelte
@@ -6,30 +6,36 @@
 <section class="pane flex flex-col items-center gap-y-2">
     <div class="flex gap-x-4">
         <LabeledNumber
-            label="Health"
-            value={$data.health}
+            label="HP"
+            bind:value={$data.health}
+            bind:max={$data.max_health}
             input_style="h-16 w-32"
         />
         <LabeledNumber
-            label="Temp. Health"
-            value={$data.temp_health}
+            label="Temporary HP"
+            bind:value={$data.temp_health}
             input_style="h-16 w-32"
         />
     </div>
     <div class="flex gap-x-6 pb-1">
         <LabeledNumber
+            label="Max HP"
+            bind:value={$data.max_health}
+            input_style="aspect-square w-11"
+        />
+        <LabeledNumber
             label="Armor"
-            value={$data.armor_class}
+            bind:value={$data.armor_class}
             input_style="aspect-square w-11"
         />
         <LabeledNumber
             label="Initiative"
-            value={$data.initiative}
+            bind:value={$data.initiative}
             input_style="aspect-square w-11"
         />
         <LabeledNumber
             label="Speed"
-            value={$data.speed}
+            bind:value={$data.speed}
             input_style="aspect-square w-11"
         />
     </div>
