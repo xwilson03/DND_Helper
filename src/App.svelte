4d9eb416c502936255a50bb88f7a7d5780b93295
xwilson03 <xanderwilson2014@gmail.com>
2024-07-29 21:39:41 -0500

    adds basic equipment panel

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import NamePlate from "./components/NamePlate.svelte";
     import StatLine from "./components/stats/StatLine.svelte";
     import CombatPanel from "./components/CombatPanel.svelte";
+    import EquipmentPanel from "./components/EquipmentPanel.svelte";
 </script>
 
 <main class="flex h-screen w-screen bg-zinc-900 text-white">
@@ -9,6 +10,7 @@
         <NamePlate />
         <StatLine />
         <CombatPanel />
+        <EquipmentPanel />
         <div class="pane flex-grow"/>
     </div>
 </main>
---
+++ src/components/EquipmentPanel.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    
+</script>
+
+<section class="pane flex flex-col items-stretch">
+    <table class="border-separate border-spacing-x-0.5 border-spacing-y-1 items-stretch">
+      <thead>
+        <tr>
+          <th>Weapon</th>
+          <th>Damage</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-zinc-900 text-center">
+          <td class="rounded-l-lg"> Quarterstaff </td>
+          <td class="border-l border-r border-zinc-800" > 1d6 / 1d8*</td>
+          <td class="rounded-r-lg"> Bludgeoning</td>
+        </tr>
+        <tr class="bg-zinc-900 text-center">
+          <td class="rounded-l-lg"> Javelin </td>
+          <td class="border-l border-r border-zinc-800" > 1d6</td>
+          <td class="rounded-r-lg"> Piercing</td>
+        </tr>
+    </tbody>
+    </table>
+</section>
