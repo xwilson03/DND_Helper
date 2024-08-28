e8ef6019b582421daf7d261077859ab68b9cd87f
xwilson03 <xanderwilson2014@gmail.com>
2024-08-27 21:01:47 -0500

    readds divider component as SplitPane child

--- src/components/SplitPane.svelte
+++ src/components/SplitPane.svelte
@@ -1,37 +1,18 @@
-<script lang="ts">
-
-    var isDragging: boolean = false;
-
-    function startDrag() {
-        isDragging = true;
-        document.addEventListener("mousemove", drag);
-        document.addEventListener("mouseup", stopDrag);
-    }
-    
-    function stopDrag() {
-        isDragging = false;
-        document.removeEventListener("mousemove", drag);
-        document.removeEventListener("mouseup", stopDrag);
-    }
-
-    function drag(event: MouseEvent) {
-        console.log(event.movementX);
-    }
+<script>
+    import Divider from "./Divider.svelte";
 
 </script>
 
 <div class="flex h-full w-full">
     <slot name="a"/>
+
     {#if $$slots.b}
-        <div class="w-[2px] bg-zinc-600">
-            <div class="relative -left-[50%] h-full w-[200%] cursor-col-resize" on:mousedown={startDrag}/>
-        </div>
+        <Divider/>
         <slot name="b"/>
     {/if}
+    
     {#if $$slots.c}
-        <div class="w-[2px] bg-zinc-600">
-            <div class="relative -left-[50%] h-full w-[200%] cursor-col-resize" on:mousedown={startDrag}/>
-        </div>
+        <Divider/>
         <slot name="c"/>
     {/if}
 </div>
