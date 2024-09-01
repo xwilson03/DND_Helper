b55634c94d3f1c7548eb2207941c54a004c87b8e
xwilson03 <xanderwilson2014@gmail.com>
2024-08-31 23:01:24 -0500

    moves placeholder spell data to CharacterData

--- src/components/SpellList.svelte
+++ src/components/SpellList.svelte
@@ -1,74 +1,9 @@
 <script lang="ts">
-
-    const allSpells = [
-        { level: '1st', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '2nd', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '3rd', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '4th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '5th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '6th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '7th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '8th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-        
-        { level: '9th', spells: [
-            {name: "temp_spell_1", description: "temp_description_1"},
-            {name: "temp_spell_2", description: "temp_description_2"},
-            {name: "temp_spell_3", description: "temp_description_3"},
-            {name: "temp_spell_4", description: "temp_description_4"},
-        ]},
-    ];
-    
+    import {character_data as data} from "@/lib/CharacterData"
 </script>
 
 <section class="pane flex flex-col items-stretch gap-y-4 overflow-scroll">
-    {#each allSpells as spellLevel}
+    {#each $data.spells as spellLevel}
         <table class="border-separate border-spacing-x-4 border-spacing-y-2 items-stretch">
             <th>{spellLevel.level} Level</th>
 
--- src/lib/CharacterData.ts
+++ src/lib/CharacterData.ts
@@ -1,5 +1,10 @@
 import { writable, type Writable } from "svelte/store";
 
+interface SpellList {
+    level: string,
+    spells: {name: string, description: string}[]
+}
+
 export interface CharacterData {
     // Metadata
     name: string
@@ -22,6 +27,9 @@ export interface CharacterData {
     intelligence: number
     wisdom: number
     charisma: number
+
+    // Spells
+    spells: SpellList[]
 };
 
 export const character_data: Writable<CharacterData> = writable({
@@ -45,5 +53,71 @@ export const character_data: Writable<CharacterData> = writable({
     constitution: 1,
     intelligence: 1,
     wisdom: 1,
-    charisma: 1
+    charisma: 1,
+
+    // Spells
+    spells: [
+        { level: '1st', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '2nd', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '3rd', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '4th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '5th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '6th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '7th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '8th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+        
+        { level: '9th', spells: [
+            {name: "temp_spell_1", description: "temp_description_1"},
+            {name: "temp_spell_2", description: "temp_description_2"},
+            {name: "temp_spell_3", description: "temp_description_3"},
+            {name: "temp_spell_4", description: "temp_description_4"},
+        ]},
+    ],
 });
